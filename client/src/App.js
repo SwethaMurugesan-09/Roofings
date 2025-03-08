a025b7a15137971bb30ffaf46888fe0a97f9e150
Swetha Murugesan <swethamk45@gmail.com>
2025-03-08 23:12:40 +0530

    updated client

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Router  } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./Pages/Home";
+import Product from './Pages/Product'
+import ProductDetials from "./Pages/ProductDetials";
+import Contact from "./Pages/Contact";
+import Toppicks from "./Pages/Toppicks";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/products" element={<Product/>}/>
+        <Route path="/products/:id" element={<ProductDetials/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/favourites" element={<Toppicks/>}/>
+      </Routes>
+      <Footer/>
+    </>
+    </div>
+  );
+}
+
+export default App;
